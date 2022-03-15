57865e31a3343ab8de5c40f03cb7f1ccdbbba8ce
rachelshapiro <rachel24@sas.upenn.edu>
2022-03-14 22:30:59 -0400

    java exercise

---
+++ exercises/javascript-2/main.js
@@ -0,0 +1,118 @@
+let button = document.querySelector(".button1");
+let buttons = document.querySelectorAll(".button1")
+let color = ["#000000"];
+let body = document.querySelector("body");
+
+console.log(button);
+console.log(buttons);
+
+function deleteTask(){
+	/*button.forEach(function (body){*/
+		button.classList.add("hide")
+	}
+
+button.addEventListener("click", deleteTask);
+
+
+let button2 = document.querySelector(".button2");
+let buttons2 = document.querySelectorAll(".button2")
+
+console.log(button2);
+console.log(buttons2);
+
+function deleteTask2(){
+	/*button.forEach(function (body){*/
+		button2.classList.add("hide")
+}
+
+button2.addEventListener("click", deleteTask2);
+
+let button3 = document.querySelector(".button3");
+let buttons3 = document.querySelectorAll(".button3")
+
+console.log(button3);
+console.log(buttons3);
+
+function deleteTask3(){
+	/*button.forEach(function (body){*/
+		button3.classList.add("hide")
+}
+
+button3.addEventListener("click", deleteTask3);
+
+let button4 = document.querySelector(".button4");
+let buttons4 = document.querySelectorAll(".button4")
+
+console.log(button4);
+console.log(buttons4);
+
+function deleteTask4(){
+	/*button.forEach(function (body){*/
+		button4.classList.add("hide")
+}
+
+button4.addEventListener("click", deleteTask4);
+
+let button5 = document.querySelector(".button5");
+let buttons5 = document.querySelectorAll(".button5")
+
+console.log(button5);
+console.log(buttons5);
+
+function deleteTask5(){
+	/*button.forEach(function (body){*/
+		button5.classList.add("hide")
+}
+
+button5.addEventListener("click", deleteTask5);
+
+
+let button6 = document.querySelector(".button6");
+let buttons6 = document.querySelectorAll(".button6")
+
+console.log(button6);
+console.log(buttons6);
+
+function deleteTask6(){
+	/*button.forEach(function (body){*/
+		button6.classList.add("hide")
+}
+
+button6.addEventListener("click", deleteTask6);
+
+
+let button7 = document.querySelector(".button7");
+let buttons7 = document.querySelectorAll(".button7")
+
+console.log(button7);
+console.log(buttons7);
+
+function deleteTask7(){
+	/*button.forEach(function (body){*/
+		button7.classList.add("hide")
+}
+
+button7.addEventListener("click", deleteTask7);
+
+let button8 = document.querySelector(".button8");
+let buttons8 = document.querySelectorAll(".button8")
+
+console.log(button8);
+console.log(buttons8);
+
+function deleteTask8(){
+	/*button.forEach(function (body){*/
+		button8.classList.add("hide")
+}
+
+button8.addEventListener("click", deleteTask8);
+
+
+
+
+
+
+
+
+
+
